d46ac8bed158f88ac278b143fd133ee7cd5c3874
yipuzhao <yipu.zhao@gatech.edu>
2018-05-19 17:59:12 -0400

    ready to run svo2 mono

--- Run_EuRoC_Mono.py
+++ Run_EuRoC_Mono.py
@@ -5,10 +5,10 @@ import subprocess
 import time
 import signal
 
-SeqNameList = ['MH_01_easy'];
+# SeqNameList = ['MH_01_easy'];
 # SeqNameList = ['V1_03_difficult', 'V2_02_medium', 'V2_03_difficult'];
-# SeqNameList = ['MH_01_easy', 'MH_02_easy', 'MH_03_medium', 'MH_04_difficult', 'MH_05_difficult', 'V1_01_easy', 'V1_02_medium', 'V1_03_difficult', 'V2_01_easy', 'V2_02_medium', 'V2_03_difficult'];
-Number_GF_List = [400, 600, 800, 1000, 1500, 2000]; # [100];
+SeqNameList = ['MH_01_easy', 'MH_02_easy', 'MH_03_medium', 'MH_04_difficult', 'MH_05_difficult', 'V1_01_easy', 'V1_02_medium', 'V1_03_difficult', 'V2_01_easy', 'V2_02_medium', 'V2_03_difficult'];
+Number_GF_List = [400, 600, 800, 1000, 1500, 2000]; # [2000]; # 
 Result_root = '/mnt/DATA/tmp/EuRoC/SVO2_Mono/'
 Num_Repeating = 10 # 20 #  5 # 
 SleepTime = 5
@@ -45,10 +45,10 @@ for ri, num_gf in enumerate(Number_GF_List):
             File_rosbag  = '/mnt/DATA/Datasets/EuRoC_dataset/BagFiles/' + SeqName + '.bag'
 
             # rosrun ORB_SLAM2 Mono PATH_TO_VOCABULARY PATH_TO_SETTINGS_FILE
-            cmd_slam   = str('LD_PRELOAD=~/svo_install_ws/install/lib/libgflags.so.2.2.0 roslaunch svo_ros ' + 'euroc_mono_lmk ' + str(int(num_gf)) + '.launch')
+            cmd_slam   = str('LD_PRELOAD=~/svo_install_ws/install/lib/libgflags.so.2.2.0 roslaunch svo_ros ' + 'euroc_mono_lmk' + str(int(num_gf)) + '.launch')
             cmd_record = str('rosbag record -O ' + Experiment_dir + '/' + SeqName + '_tf /tf __name:=rec_bag')
             cmd_timelog = str('cp /home/yipuzhao/svo_install_overlay_ws/tmpLog.txt ' + Experiment_dir + '/' + SeqName + '_Log.txt')
-            cmd_rosbag = 'rosbag play ' + File_rosbag + ' -u 30' # + ' -r 0.3'
+            cmd_rosbag = 'rosbag play ' + File_rosbag # + ' -u 30' # + ' -r 0.3'
             print bcolors.WARNING + "cmd_slam: \n"   + cmd_slam   + bcolors.ENDC
             print bcolors.WARNING + "cmd_record: \n" + cmd_record + bcolors.ENDC
             print bcolors.WARNING + "cmd_timelog: \n" + cmd_timelog + bcolors.ENDC
@@ -70,7 +70,12 @@ for ri, num_gf in enumerate(Number_GF_List):
 
             print bcolors.OKGREEN + "Finished rosbag playback, kill the process" + bcolors.ENDC
             subprocess.call('rosnode kill /rec_bag', shell=True)
-            subprocess.call('rosnode kill svo_ros', shell=True)
+            subprocess.call('rosnode kill /svo', shell=True)
+            # subprocess.call('pkill roslaunch', shell=True)
+            # subprocess.call('pkill svo_node', shell=True)
+
+            print bcolors.OKGREEN + "Sleeping for a few secs to wait for svo quit" + bcolors.ENDC
+            time.sleep(SleepTime)
             print bcolors.OKGREEN + "Copy the time log to result folder" + bcolors.ENDC
             subprocess.call(cmd_timelog, shell=True)
             # proc_rec.terminate()
