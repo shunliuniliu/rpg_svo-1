634d9d0b31dc15343c4426fce6a54b29efd50120
euclid <yipu.zhao@gatech.edu>
2018-06-08 15:40:44 -0400

    update log path for Euclid

--- Run_EuRoC_Mono.py
+++ Run_EuRoC_Mono.py
@@ -5,11 +5,14 @@ import subprocess
 import time
 import signal
 
-# SeqNameList = ['MH_01_easy'];
+# SeqNameList = ['MH_03_medium', 'MH_04_difficult', 'not_exist'];
 # SeqNameList = ['V1_03_difficult', 'V2_02_medium', 'V2_03_difficult'];
 SeqNameList = ['MH_01_easy', 'MH_02_easy', 'MH_03_medium', 'MH_04_difficult', 'MH_05_difficult', 'V1_01_easy', 'V1_02_medium', 'V1_03_difficult', 'V2_01_easy', 'V2_02_medium', 'V2_03_difficult'];
+
+Result_root = '/mnt/DATA/Euclid_Test/SVO2_Baseline/'
+# Result_root = '/mnt/DATA/tmp/EuRoC/SVO2_Mono/'
+
 Number_GF_List = [400, 600, 800, 1000, 1500, 2000]; # [2000]; # 
-Result_root = '/mnt/DATA/tmp/EuRoC/SVO2_Mono/'
 Num_Repeating = 10 # 20 #  5 # 
 SleepTime = 5
 
@@ -30,7 +33,6 @@ for ri, num_gf in enumerate(Number_GF_List):
 
     for iteration in range(0, Num_Repeating):
 
-        # Experiment_dir = Result_root + Experiment_prefix + '_Round' + str(iteration + 1)
         Experiment_dir = Result_root + Experiment_prefix + '_Round' + str(iteration + 1)
         cmd_mkdir = 'mkdir ' + Experiment_dir
         subprocess.call(cmd_mkdir, shell=True)
